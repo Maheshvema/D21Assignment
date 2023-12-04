ec0cc29e6ea3e769c1c789e574cc2437f4f627fc
Maheshvema <babumahesh2088@gmail.com>
2023-12-04 15:06:29 +0530

    modified

---
+++ SeriDesiBinaXml/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace SeriDesiBinaXml
+{
+    [Serializable]
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Salary { get; set; }
+    }
+}
